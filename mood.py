5677006ab6ba150c9a8ef8f15cc72e8a40a904ae
Meesha <44530786+meesha7@users.noreply.github.com>
2020-05-12 10:43:34 +0200

    Check for __name__

--- mood.py
+++ mood.py
@@ -8,17 +8,18 @@ import argparse
 import time
 import ds
 
-DEFAULT_OUTPUT_NAME = 'daylio-plot-{}.png'.format(time.strftime('%Y-%m-%d-%H%M%S'))
+if __name__ == '__main__':
+    DEFAULT_OUTPUT_NAME = 'daylio-plot-{}.png'.format(time.strftime('%Y-%m-%d-%H%M%S'))
 
-parser = argparse.ArgumentParser()
-parser.add_argument('path', type=str, help='Path to the Dailyo export')
-parser.add_argument('--output', '-o', type=str, default=DEFAULT_OUTPUT_NAME,
-                    help='Ouptut path for the plot')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str, help='Path to the Dailyo export')
+    parser.add_argument('--output', '-o', type=str, default=DEFAULT_OUTPUT_NAME,
+                        help='Ouptut path for the plot')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-loader = ds.data.DataLoader(args.path)
-avg_moods = loader.load()
+    loader = ds.data.DataLoader(args.path)
+    avg_moods = loader.load()
 
-plot = ds.plot.Plot(avg_moods)
-plot.plot_average_moods(args.output)
+    plot = ds.plot.Plot(avg_moods)
+    plot.plot_average_moods(args.output)
