5852af6f652152f053d5969dd1d8ac704c2c4ae3
Meesha <44530786+meesha7@users.noreply.github.com>
2020-05-15 18:31:36 +0200

    Use settings for dev only

--- dayliostats/settings.py
+++ dayliostats/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['DS_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -121,9 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 SECURE_REFERRER_POLICY = 'same-origin'
