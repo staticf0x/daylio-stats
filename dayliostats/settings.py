5dae47bedb6a456a911fec2f39800e6514f0ebeb
Meesha <44530786+meesha7@users.noreply.github.com>
2020-05-27 18:26:18 +0200

    Another try on staticfiles

--- dayliostats/settings.py
+++ dayliostats/settings.py
@@ -124,5 +124,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'ds', 'static'),
 )
