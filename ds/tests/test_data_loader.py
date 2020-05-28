e2a7a9474ab832fa61b93e7d373f0f6c2d14a3f9
Meesha <44530786+meesha7@users.noreply.github.com>
2020-05-28 18:20:08 +0200

    Add datetime to test_load_csv

--- ds/tests/test_data_loader.py
+++ ds/tests/test_data_loader.py
@@ -4,6 +4,7 @@ Test the DataLoader class
 """
 
 import os
+import datetime
 from django.test import TestCase
 from ds.lib.data import DataLoader
 from dayliostats.settings import BASE_DIR
@@ -14,5 +15,11 @@ class TestsDataLoader(TestCase):
         dl = DataLoader(os.path.join(BASE_DIR, 'ds', 'tests', 'data', 'test_data.csv'))
         data = dl.load()
 
-        self.assertEquals(data[0][1], 2)  # First entry avg mood
-        self.assertEquals(data[-1][1], 4.6)  # Last entry avg mood
+        first_day = data[0]
+        last_day = data[-1]
+
+        self.assertEquals(first_day[0], datetime.datetime(2020, 5, 25))
+        self.assertEquals(first_day[1], 2)
+
+        self.assertEquals(last_day[0], datetime.datetime(2020, 5, 28))
+        self.assertEquals(last_day[1], 4.6)
