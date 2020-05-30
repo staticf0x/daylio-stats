bd64cce5e0f8f5a854b030507875861b3c2b5af9
Meesha <44530786+meesha7@users.noreply.github.com>
2020-05-30 20:29:39 +0200

    Add postgresql config

--- dayliostats/settings.py
+++ dayliostats/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,13 +79,20 @@ WSGI_APPLICATION = 'dayliostats.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# TODO: Use postgres
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dayliostats',
+        'USER': 'dayliostats',
+        'PASSWORD': os.environ['DS_DB_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+if sys.argv[1] == 'test':
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+    DATABASES['default']['NAME'] = ':memory:'
 
 
 # Password validation
