4a8d83323bf332bc456f234b28c16f97349ad9bf
Meesha <44530786+meesha7@users.noreply.github.com>
2020-06-27 16:55:53 +0200

    Print whether a mood period is recent

--- ds/management/commands/stats.py
+++ ds/management/commands/stats.py
@@ -4,9 +4,12 @@
 Look for high and low periods in the data
 """
 
+import datetime
 from django.core.management.base import BaseCommand
 import ds.lib
 import numpy as np
+
+OUTPUT_FMT = '{} — {}, {:2d} days, avg: {:.2f}{}'
 
 
 class Command(BaseCommand):
@@ -29,16 +32,32 @@ class Command(BaseCommand):
         print(f'Average mood: {mean:.2f} ± {std:.2f}')
         print()
 
+        today = datetime.datetime.now()
+
         print('Highs:')
         for period in stats.find_high_periods():
-            print('{} — {}, {:2d} days, avg: {:.2f}'.format(period.start_date.strftime('%d/%m/%Y'),
-                                                            period.end_date.strftime('%d/%m/%Y'),
-                                                            period.duration,
-                                                            period.avg_mood))
+            until_today = today - period.end_date
+            recent = ''
+
+            if until_today.days < 14:
+                recent = f' ({until_today.days} days ago)'
+
+            print(OUTPUT_FMT.format(period.start_date.strftime('%d/%m/%Y'),
+                                    period.end_date.strftime('%d/%m/%Y'),
+                                    period.duration,
+                                    period.avg_mood,
+                                    recent))
 
         print('\nLows:')
         for period in stats.find_low_periods():
-            print('{} — {}, {:2d} days, avg: {:.2f}'.format(period.start_date.strftime('%d/%m/%Y'),
-                                                            period.end_date.strftime('%d/%m/%Y'),
-                                                            period.duration,
-                                                            period.avg_mood))
+            until_today = today - period.end_date
+            recent = ''
+
+            if until_today.days < 14:
+                recent = f' ({until_today.days} days ago)'
+
+            print(OUTPUT_FMT.format(period.start_date.strftime('%d/%m/%Y'),
+                                    period.end_date.strftime('%d/%m/%Y'),
+                                    period.duration,
+                                    period.avg_mood,
+                                    recent))
