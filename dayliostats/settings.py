b002aaeb7e2b7d5c99358cfee5bbdaa44c39a724
Meesha <44530786+meesha7@users.noreply.github.com>
2021-04-06 01:32:26 +0200

    Finish Dockerfile

--- dayliostats/settings.py
+++ dayliostats/settings.py
@@ -17,7 +17,7 @@ import dotenv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-dotenv.read_dotenv(BASE_DIR, '.env')
+dotenv.read_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
