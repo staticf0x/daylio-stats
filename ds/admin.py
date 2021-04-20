ae0438741e9b6af142d109fece8a106492c51a39
Meesha <44530786+meesha7@users.noreply.github.com>
2021-04-20 10:36:01 +0200

    Add basic import

--- ds/admin.py
+++ ds/admin.py
@@ -7,4 +7,5 @@ from django.contrib import admin
 from ds import models
 
 admin.site.register(models.UserSettings)
+admin.site.register(models.Activity)
 admin.site.register(models.Entry)
--- ds/models.py
+++ ds/models.py
@@ -1,3 +1,5 @@
+"""App models."""
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -8,12 +10,33 @@ class UserSettings(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     save_notes = models.BooleanField(default=True)
 
+    def __str__(self):
+        return f'UserSettings ({self.id}, {self.user})'
+
+
+class Activity(models.Model):
+    """
+    Activity for an Entry for a User.
+    The reason there's a User reference is for the situation
+    when a user wants to delete all their data (incl. activities).
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'Activity ({self.id}, {self.user}): {self.name}'
+
 
 class Entry(models.Model):
-    """Journal entry"""
+    """Journal entry."""
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     datetime = models.DateTimeField()
-    mood = models.CharField(max_length=64)
-    activities = models.CharField(max_length=256)
+    mood_name = models.CharField(max_length=64)
+    mood = models.IntegerField()
+    activities = models.ManyToManyField(Activity)
     notes = models.TextField()
+
+    def __str__(self):
+        return f'Entry ({self.id}, {self.user}): {self.datetime} {self.mood_name}'
