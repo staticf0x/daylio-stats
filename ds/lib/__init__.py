5df7defb34adbce9ca9a3f0e1de7383d176cc743
Meesha <44530786+meesha7@users.noreply.github.com>
2021-04-23 16:59:12 +0200

    Add decorator for API endpoints behind login

--- ds/lib/__init__.py
+++ ds/lib/__init__.py
@@ -3,4 +3,29 @@
 Lib for the project
 """
 
+from django.http import JsonResponse
+
 from . import data, entries, plot
+
+
+def api_login_required(view):
+    """
+    Decorator for requiring user login for API endpoints.
+    Instead of redirecting to a login view, this will return
+    a JSON object with an error:
+    {
+        "status": "error",
+        "message": "..."
+    }
+    """
+
+    def inner(request):
+        if not request.user.is_authenticated:
+            return JsonResponse(
+                {'status': 'error', 'message': 'Not authenticated'},
+                status=403
+            )
+
+        return view(request)
+
+    return inner
