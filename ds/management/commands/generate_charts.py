567cf9868aa1cbac8c74fa3737aba9ff754d4cf3
Meesha <44530786+meesha7@users.noreply.github.com>
2021-09-12 12:23:11 +0200

    Add option for config file in generate_charts command

--- ds/management/commands/generate_charts.py
+++ ds/management/commands/generate_charts.py
@@ -4,11 +4,12 @@
 A command to load the data and plot them manually
 """
 
+import json
 import os
 import time
 
+from daylio_parser.config import MoodConfig
 from daylio_parser.parser import Parser
-from daylio_parser.stats import Stats
 from django.core.management.base import BaseCommand
 
 import ds.lib
@@ -21,20 +22,38 @@ class Command(BaseCommand):
         DEFAULT_OUTPUT_NAME = 'daylio-plot-{}.png'.format(time.strftime('%Y-%m-%d-%H%M%S'))
 
         parser.add_argument('path', type=str, help='Path to the Daylio export')
-        parser.add_argument('--output', '-o', type=str, default=DEFAULT_OUTPUT_NAME,
-                            help='Ouptut path for the plot')
+        parser.add_argument(
+            '--output', '-o', type=str, default=DEFAULT_OUTPUT_NAME, help='Ouptut path for the plot'
+        )
+        parser.add_argument('--config', '-c', type=str, help='Path to the config file')
 
     def handle(self, *args, **kwargs):
         if not os.path.exists(kwargs['path']):
             print(f'Path: {kwargs["path"]} doesn\'t exist')
             return
 
+        print('Loading config...')
+
+        if kwargs['config']:
+            if not os.path.exists(kwargs['config']):
+                print(f'Config file: {kwargs["config"]} doesn\'t exist')
+                return
+
+            with open(kwargs['config'], 'r') as fread:
+                data = json.load(fread)
+
+                moods = data.get('moods')
+                colors = data.get('colors')
+
+                config = MoodConfig(moods, colors)
+        else:
+            config = MoodConfig()
+
         print('Loading data...')
 
-        parser = Parser()
+        parser = Parser(config)
         entries = parser.load_csv(kwargs['path'])
 
-        stats = Stats(entries)
         plots = (5, 10)
 
         print('Generating charts...')
