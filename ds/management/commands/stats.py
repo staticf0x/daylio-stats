671975643e22dbe8beb07ebb0b82aa6d5236e4ea
staticf0x <44530786+staticf0x@users.noreply.github.com>
2022-05-01 16:49:28 +0200

    Add config to activities and stats commands

--- ds/management/commands/stats.py
+++ ds/management/commands/stats.py
@@ -1,8 +1,10 @@
 """Look for high and low periods in the data."""
 
 import datetime
+import json
 import os
 
+from daylio_parser.config import MoodConfig
 from daylio_parser.parser import Parser
 from daylio_parser.stats import Stats
 from django.core.management.base import BaseCommand
@@ -11,21 +13,41 @@ OUTPUT_FMT = "{} — {}, {:2d} days, avg: {:.2f}{}"
 
 
 class Command(BaseCommand):
+    """Command to generate additional stats."""
+
     help = "Generate additional stats"
 
     def add_arguments(self, parser):
+        """Add command arguments."""
         parser.add_argument("path", type=str, help="Path to the Daylio export")
+        parser.add_argument("--config", "-c", type=str, help="Path to the config file")
 
     def handle(self, *args, **kwargs):
+        """Run the command."""
         if not os.path.exists(kwargs["path"]):
             print(f'Path: {kwargs["path"]} doesn\'t exist')
             return
 
+        if kwargs["config"]:
+            if not os.path.exists(kwargs["config"]):
+                print(f'Config file: {kwargs["config"]} doesn\'t exist')
+                return
+
+            with open(kwargs["config"], "r") as fread:
+                data = json.load(fread)
+
+                moods = data.get("moods")
+                colors = data.get("colors")
+
+                config = MoodConfig(moods, colors)
+        else:
+            config = MoodConfig()
+
         # Load the data
-        parser = Parser()
+        parser = Parser(config)
         entries = parser.load_csv(kwargs["path"])
 
-        stats = Stats(entries)
+        stats = Stats(entries, config)
 
         mean, std = stats.mean()
 
