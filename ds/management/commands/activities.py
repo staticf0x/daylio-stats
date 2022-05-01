123b5f0a10be401a8e5ef224cd65283073a24c59
staticf0x <44530786+staticf0x@users.noreply.github.com>
2022-05-01 16:49:28 +0200

    Add config to activities and stats commands

--- ds/management/commands/activities.py
+++ ds/management/commands/activities.py
@@ -1,25 +1,47 @@
 """Show a list of activities from best to worst."""
 
+import json
 import os
 
+from daylio_parser.config import MoodConfig
 from daylio_parser.parser import Parser
 from daylio_parser.stats import Stats
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = "Generate additional stats"
+    """Command to display activity stats."""
+
+    help = "Generate activity stats"
 
     def add_arguments(self, parser):
+        """Add command arguments."""
         parser.add_argument("path", type=str, help="Path to the Daylio export")
+        parser.add_argument("--config", "-c", type=str, help="Path to the config file")
 
     def handle(self, *args, **kwargs):
+        """Run the command."""
         if not os.path.exists(kwargs["path"]):
             print(f'Path: {kwargs["path"]} doesn\'t exist')
             return
 
+        if kwargs["config"]:
+            if not os.path.exists(kwargs["config"]):
+                print(f'Config file: {kwargs["config"]} doesn\'t exist')
+                return
+
+            with open(kwargs["config"], "r") as fread:
+                data = json.load(fread)
+
+                moods = data.get("moods")
+                colors = data.get("colors")
+
+                config = MoodConfig(moods, colors)
+        else:
+            config = MoodConfig()
+
         # Load the data
-        parser = Parser()
+        parser = Parser(config)
         entries = parser.load_csv(kwargs["path"])
 
         stats = Stats(entries)
